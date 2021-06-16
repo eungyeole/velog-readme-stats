4d85e4fd917500cfa3af78797d591d40b66eb2d6
Eungyeol <eungyeole@naver.com>
2021-06-16 14:53:12 +0900

    first commit

---
+++ src/apis/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { request } = require('graphql-request');
+const createCard = require('../cards/new-log');
+const card = require('../cards/new-log');
+const app = express()
+const port = 3000;
+app.get('/api', async (req, res)=>{
+    res.setHeader('Content-Type', 'image/svg+xml');
+    const endpoint = "https://v2.velog.io/graphql";
+    const query = `
+        query Posts($cursor: ID, $username: String, $temp_only: Boolean, $tag: String, $limit: Int) {
+            posts(cursor: $cursor, username: $username, temp_only: $temp_only, tag: $tag, limit: $limit) {
+            id
+            title
+            short_description
+            thumbnail
+            user {
+                username
+                profile {
+                thumbnail
+                }
+            }
+            url_slug
+            released_at
+            updated_at
+            comments_count
+            tags
+            likes
+            }
+        }
+    `;
+    const variables = {
+        "username": req.query.name,
+        "limit" : 1,
+        "tag" : req.query.tag
+    }
+    const { posts } = await request(endpoint, query, variables);
+    if(posts.length > 0) res.send(createCard(posts[0]))
+    else res.send("Not Found Post");
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
