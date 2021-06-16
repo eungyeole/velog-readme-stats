2b57c803b5da5e3eada2232e093069bf35d962b1
Eungyeol <eungyeole@naver.com>
2021-06-16 21:58:10 +0900

    fix: Documnet error

--- src/cards/new-log.js
+++ src/cards/new-log.js
@@ -1,3 +1,4 @@
+const { koCheck } = require("../utils")
 
 
 const createCardTitle = (username, likes) => {
@@ -35,8 +36,8 @@ const createCardBottom = ({tags}) => {
             ${
                 tags.map((element) => {
                     const text = element.replaceAll(' ','');
-                    // const blankSize = element.length-text.length;
-                    const size=true ? text.length*12+12 : text.length*9+5; //+ blankSize*2;
+                    const blankSize = element.length-text.length;
+                    const size=koCheck(text) ? text.length*12+12 : text.length*9+5; //+ blankSize*2;
                     if(prev+size > 400) return;
                     else prev+=size+5;
                     return`
