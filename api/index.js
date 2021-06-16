6d713a59c7282858c395149d77acfecee0556aad
Eungyeol <eungyeole@naver.com>
2021-06-16 21:40:38 +0900

    fix

--- api/index.js
+++ api/index.js
@@ -1,7 +1,5 @@
 const createCard = require('../src/cards/new-log');
 const fetchPost = require('../src/fetchers/post-fetcher');
-// const express  = require('express');
-// const app = express();
 
 module.exports = async (req, res) => {
     const { name, tag } = req.query;
@@ -13,19 +11,3 @@ module.exports = async (req, res) => {
         return res.send(e.message)
     }
 }
-
-// app.get('/api',async (req, res)=> {
-//     const { name, tag } = req.query;
-//     res.setHeader('Content-Type', 'image/svg+xml');
-//     try{
-//         const post = await fetchPost(name, tag);
-//         return res.send(createCard(post))
-//     } catch(e){
-//         return res.send(e.message)
-//     }
-// })
-
-
-// app.listen(3000,()=>{
-//     console.log('test');
-// })
