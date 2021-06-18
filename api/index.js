29e9132e128a2982076916ec3902de520382ca10
Eungyeole <eungyeole@naver.com>
2021-06-18 09:03:05 +0900

    add : 다크모드

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const createCard = require('../src/cards/new-log');
+const createCardDark = require('../src/cards/new-log-black');
 const fetchPost = require('../src/fetchers/post-fetcher');
 
 
 module.exports = async (req, res) => {
-    const { name, tag } = req.query;
+    const { name, tag, background } = req.query;
     res.setHeader('Content-Type', 'image/svg+xml');
     try{
         const post = await fetchPost(name, tag);
-        return res.send(createCard(post))
+        return res.send(background==='dark' ? createCardDark(post) : createCard(post))
     } catch(e){
         return res.send(e.message)
     }
