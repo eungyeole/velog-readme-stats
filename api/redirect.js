4a9e842f999dcb1d87b51f372e94b453c36c8b6a
Eungyeole <eungyeole@naver.com>
2021-06-21 11:59:28 +0900

    fix : Redirect Error

--- api/redirect.js
+++ api/redirect.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     try{
         const post = await fetchPost(name, tag);
         const url = new String(`https://velog.io/@${post.user.username}/${post.url_slug}`)
-        res.redirect(url)
+        res.redirect(200, url)
         return 
     } catch(e){
         return res.send(e.message)
