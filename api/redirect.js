a5ab07df2e3641acea8057dcb1603055117eb12d
Eungyeole <eungyeole@naver.com>
2021-06-21 11:22:55 +0900

    Create : page redirect 기능 추가

---
+++ api/redirect.js
@@ -0,0 +1,15 @@
+const fetchPost = require('../src/fetchers/post-fetcher');
+
+
+module.exports = async (req, res) => {
+    const { name, tag } = req.query;
+    try{
+        const post = await fetchPost(name, tag);
+        const url = new String(`https://velog.io/@${post.user.username}/${post.url_slug}`)
+        res.setHeader('Location', url);
+        res.send('loading...');
+        return 
+    } catch(e){s
+        return res.send(e.message)
+    }
+}
