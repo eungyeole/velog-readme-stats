2431eb619afb37a593a1ae33945cfe5fa4134a58
Eungyeole <eungyeole@naver.com>
2021-06-21 12:03:58 +0900

    fix: Unexpected token 에러 수정

--- api/redirect.js
+++ api/redirect.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
     try{
         const post = await fetchPost(name, tag);
         const url = new String(`https://velog.io/@${post.user.username}/${post.url_slug}`)
-        res.send(`<script>window.location.href=${url}</script>`);
+        res.send(`<script>window.location.href='${url}'</script>`);
         return 
     } catch(e){
         return res.send(e.message)
