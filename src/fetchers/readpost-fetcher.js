91c22d0df8cbae97f949e643008c1005d3ea9275
Eungyeole <eungyeole@naver.com>
2021-06-30 11:03:04 +0900

    update: url_slug를 통해 검색

---
+++ src/fetchers/readpost-fetcher.js
@@ -0,0 +1,43 @@
+const { request } = require("../utils")
+
+const fetcher = (variables) => {
+    return request(
+        {
+            query: `
+            query ReadPost($username: String, $url_slug: String) {
+                post(username: $username, url_slug: $url_slug) {
+                  id
+                  title
+                  short_description
+                  thumbnail
+                  user {
+                    username
+                    profile {
+                      thumbnail
+                    }
+                  }
+                  url_slug
+                  released_at
+                  updated_at
+                  comments_count
+                  tags
+                  likes
+                }
+              }
+            `,
+            variables
+        }
+    )
+}
+
+async function fetchReadPost(name, slug) {
+    try{
+        const { data } = await fetcher({"username": name, "url_slug" : slug});
+        return data.data.posts[0];
+    }catch(e){
+        throw new Error(e)
+    }
+
+}
+
+module.exports=fetchReadPost;
