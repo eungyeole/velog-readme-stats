0534bfca9055e4856cb02205a2d5cc301b3145cb
안은결 <eungyeole@naver.com>
2022-02-16 14:48:37 +0900

    fix: test

--- api/index.js
+++ api/index.js
@@ -1,16 +1,17 @@
-const createCard = require('../src/cards/new-log');
-const createCardDark = require('../src/cards/new-log-black');
-const fetchPost = require('../src/fetchers/post-fetcher');
-const fetchReadPost = require('../src/fetchers/readpost-fetcher');
-
+const createCard = require("../cards/new-log");
+const createCardDark = require("../cards/new-log-black");
+const fetchPost = require("../fetchers/post-fetcher");
+const fetchReadPost = require("../fetchers/readpost-fetcher");
 
 module.exports = async (req, res) => {
-    const { name, tag, color, slug } = req.query;
-    res.setHeader('Content-Type', 'image/svg+xml');
-    try{
-        const post = !slug ? await fetchPost(name, tag) : await fetchReadPost(name, slug);
-        return res.send(color==='dark' ? createCardDark(post) : createCard(post))
-    } catch(e){
-        return res.send(e.message)
-    }
-}
+  const { name, tag, color, slug } = req.query;
+  res.setHeader("Content-Type", "image/svg+xml");
+  try {
+    const post = !slug
+      ? await fetchPost(name, tag)
+      : await fetchReadPost(name, slug);
+    return res.send(color === "dark" ? createCardDark(post) : createCard(post));
+  } catch (e) {
+    return res.send(e.message);
+  }
+};
---
+++ api/list.js
@@ -0,0 +1,13 @@
+const createLatestPostsCard = require("../cards/latest-posts");
+const fetchPosts = require("../fetchers/posts-fetcher");
+
+module.exports = async (req, res) => {
+  const { name } = req.query;
+  res.setHeader("Content-Type", "image/svg+xml");
+  try {
+    const posts = await fetchPosts(name);
+    return res.send(createLatestPostsCard(posts));
+  } catch (e) {
+    return res.send(e.message);
+  }
+};
